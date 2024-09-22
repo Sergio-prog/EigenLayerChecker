5e1a6956e4360b24285000c5b4344339ff7d9a50
Sergey Nesterov <sergioreklamplay@gmail.com>
2024-09-23 00:20:54 +0300

    Initial commit

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,7 +5,7 @@ from requests import Session
 
 class BaseChecker(ABC):
     """
-    Base class for EIGEN token drop check. Well, idk why I wrote it now.
+    Base class for EIGEN token drop check.
     """
     @staticmethod
     def fetch_drop_amount(address: str, session: Session = None) -> float | int:
